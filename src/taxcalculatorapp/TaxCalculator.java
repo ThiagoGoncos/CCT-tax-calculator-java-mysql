d8d00df94554290304638cef2c496332a87240ec
Thiago2022161 <146199077+Thiago2022161@users.noreply.github.com>
2023-12-19 17:32:57 +0000

    Update TaxCalculator.java
    Updating some code for this interface

--- src/taxcalculatorapp/TaxCalculator.java
+++ src/taxcalculatorapp/TaxCalculator.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package taxcalculatorapp;
 
@@ -8,6 +8,9 @@ package taxcalculatorapp;
  *
  * @author thiagogoncos
  */
-public class TaxCalculator {
-    
+public interface TaxCalculator {
+    double calculateIncomeTax(double grossIncome, double taxCredits);
+    double calculateUSC(double grossIncome);
+    double calculatePRSI(double grossIncome);
+    double calculateTotalTax(double grossIncome, double taxCredits);
 }
