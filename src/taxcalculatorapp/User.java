dffc085c170896e30ce1206d65f4eaedfecdc6b0
kelvindumas2 <146457744+kelvindumas2@users.noreply.github.com>
2023-12-19 21:01:02 +0000

    Update User.java
    Update User.java

--- src/taxcalculatorapp/User.java
+++ src/taxcalculatorapp/User.java
@@ -6,67 +6,75 @@ package taxcalculatorapp;
 
 /**
  *
- * @author thiagogoncos
+ * @author kelvindumas
  */
-abstract class User implements UserActions {
-    private String username;
-    private String password;
-    private String name;
-    private String surname;
 
-    public User(String username, String password, String name, String surname) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.surname = surname;
-    }
+public abstract class User {
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+	private UserType userType;
 
-public String getUsername() {
-        return username;
-    }
+	public User(String username, String password, String name, String surname, UserType userType) {
+    	this.username = username;
+    	this.password = password;
+    	this.name = name;
+    	this.surname = surname;
+    	this.userType = userType;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getUsername() {
+    	return username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setUsername(String username) {
+    	this.username = username;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+    	return password;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setPassword(String password) {
+    	this.password = password;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+    	return name;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public void setName(String name) {
+    	this.name = name;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    
-    @Override
-    public void modifyProfile(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-        System.out.println("User profile modified: " + this);
-    }
+	public String getSurname() {
+    	return surname;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                '}';
-    }
+	public void setSurname(String surname) {
+    	this.surname = surname;
+	}
+
+	public UserType getUserType() {
+    	return userType;
+	}
+
+	public void setUserType(UserType userType) {
+    	this.userType = userType;
+	}
+
+	public abstract void modifyProfile(String name, String surname);
+
+	public abstract String getJobRole();
+
+	@Override
+	public String toString() {
+    	return "User{" +
+            	"username='" + username + '\'' +
+            	", password='" + password + '\'' +
+            	", name='" + name + '\'' +
+            	", surname='" + surname + '\'' +
+            	'}';
+	}
 }
