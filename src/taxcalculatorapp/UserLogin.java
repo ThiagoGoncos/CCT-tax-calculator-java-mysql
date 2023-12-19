c99df168ccb457e38a7c53332e59c2f06f487d1c
Thiago2022161 <146199077+Thiago2022161@users.noreply.github.com>
2023-12-19 17:34:51 +0000

    Update UserLogin.java
    Updating some codes to UserLogin

--- src/taxcalculatorapp/UserLogin.java
+++ src/taxcalculatorapp/UserLogin.java
@@ -9,5 +9,28 @@ package taxcalculatorapp;
  * @author thiagogoncos
  */
 public class UserLogin {
-    
+    public static User loginUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.next();
+
+        System.out.print("Enter password: ");
+        String password = scanner.next();
+
+        // Verifica se o usuário admin está tentando fazer login
+        if (username.equals("CCT") && password.equals("Dublin")) {
+            return Database.getUser(username);
+        }
+
+        User authenticatedUser = Database.getUser(username);
+
+        if (authenticatedUser != null && authenticatedUser.getPassword().equals(password)) {
+            System.out.println("User login successful: " + authenticatedUser.getUsername());
+            return authenticatedUser;
+        } else {
+            System.out.println("Authentication failed. Please check your username and password.");
+            return null;
+        }
+    }
 }
