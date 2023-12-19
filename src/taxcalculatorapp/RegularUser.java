0ac9292fa41164870bfa1d005414585322c3edec
Thiago2022161 <146199077+Thiago2022161@users.noreply.github.com>
2023-12-19 21:08:33 +0000

    Update RegularUser.java

--- src/taxcalculatorapp/RegularUser.java
+++ src/taxcalculatorapp/RegularUser.java
@@ -4,76 +4,73 @@
 */
 package taxcalculatorapp;
 
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.sql.SQLException
-
-
+import java.sql.SQLException;
 
 public class RegularUser extends User {
 
-	private String jobRole;
-	private List<TaxCalculation> taxCalculations = new ArrayList<>();
+    private String jobRole;
+    private List<TaxCalculation> taxCalculations = new ArrayList<>();
 
-	public RegularUser(String username, String password, String name, String surname, String jobRole) {
-    	super(username, password, name, surname, UserType.REGULAR_USER);
-    	this.jobRole = jobRole;
-	}
+    public RegularUser(String username, String password, String name, String surname, String jobRole) {
+        super(username, password, name, surname, UserType.REGULAR_USER);
+        this.jobRole = jobRole;
+    }
 
-	public String getJobRole() {
-    	return jobRole;
-	}
+    public String getJobRole() {
+        return jobRole;
+    }
 
-	public void setJobRole(String jobRole) {
-    	this.jobRole = jobRole;
-	}
+    public void setJobRole(String jobRole) {
+        this.jobRole = jobRole;
+    }
 
-	@Override
-	public void modifyProfile(String name, String surname) {
-    	super.setName(name);
-    	super.setSurname(surname);
-    	System.out.println("RegularUser profile modified: " + this);
-	}
+    @Override
+    public void modifyProfile(String name, String surname) {
+        super.setName(name);
+        super.setSurname(surname);
+        System.out.println("RegularUser profile modified: " + this);
+    }
 
-	public TaxCalculation calculateTaxes(double grossIncome, double taxCredits) {
-    	TaxCalculator taxCalculator = new TaxCalculatorImpl();
+    public TaxCalculation calculateTaxes(double grossIncome, double taxCredits) {
+        TaxCalculator taxCalculator = new TaxCalculatorImpl();
 
-    	// Seu código para calcular os impostos
-    	double incomeTax = taxCalculator.calculateIncomeTax(grossIncome, taxCredits);
-    	double usc = taxCalculator.calculateUSC(grossIncome);
-    	double prsi = taxCalculator.calculatePRSI(grossIncome);
+        double incomeTax = taxCalculator.calculateIncomeTax(grossIncome, taxCredits);
+        double usc = taxCalculator.calculateUSC(grossIncome);
+        double prsi = taxCalculator.calculatePRSI(grossIncome);
 
-    	// Criar e retornar uma instância de TaxCalculation
-    	TaxCalculation taxCalculation = new TaxCalculation(grossIncome, taxCredits, incomeTax, usc, prsi);
-    	return taxCalculation;
-	}
+        TaxCalculation taxCalculation = new TaxCalculation(grossIncome, taxCredits, incomeTax, usc, prsi);
+        return taxCalculation;
+    }
 
-	public void saveTaxCalculations(double grossIncome, double taxCredits, double incomeTax, double usc, double prsi) {
-    	TaxCalculator taxCalculator = new TaxCalculatorImpl();
+    public void saveTaxCalculations(double grossIncome, double taxCredits) {
+        TaxCalculator taxCalculator = new TaxCalculatorImpl();
 
-    	double totalTax = taxCalculator.calculateTotalTax(grossIncome, taxCredits);
-   	 
-    	TaxCalculation taxCalculation = new TaxCalculation(grossIncome, taxCredits, incomeTax, usc, prsi);
-    	taxCalculations.add(taxCalculation);
+        double incomeTax = taxCalculator.calculateIncomeTax(grossIncome, taxCredits);
+        double usc = taxCalculator.calculateUSC(grossIncome);
+        double prsi = taxCalculator.calculatePRSI(grossIncome);
 
-    	// Salvando na tabela TaxTable
-    	DatabaseWriter.saveTaxCalculations(this.getUsername(), grossIncome, taxCredits, incomeTax, usc, prsi);
-    	System.out.println("Tax calculations saved successfully!");
-	}
+        TaxCalculation taxCalculation = new TaxCalculation(grossIncome, taxCredits, incomeTax, usc, prsi);
 
-	public List<TaxCalculation> getTaxCalculations() {
-    	return taxCalculations;
-	}
+        taxCalculations.add(taxCalculation);
 
-	public void saveTaxCalculation(TaxCalculation taxCalculation) {
-    	taxCalculations.add(taxCalculation);
-	}
+        DatabaseWriter.saveUserDataAndTaxes(this, grossIncome, taxCredits, incomeTax, usc, prsi);
+        System.out.println("Tax calculations saved successfully!");
+    }
 
-	public void saveUserToDatabase() {
-    	DatabaseWriter writer = new DatabaseWriter();
-    	writer.addUser(this);
-	}
+    public List<TaxCalculation> getTaxCalculations() {
+        return taxCalculations;
+    }
+
+    public void saveTaxCalculation(TaxCalculation taxCalculation) {
+        taxCalculations.add(taxCalculation);
+    }
+
+    public void saveUserToDatabase() {
+        DatabaseWriter writer = new DatabaseWriter();
+        writer.addUser(this);
+    }
 }
