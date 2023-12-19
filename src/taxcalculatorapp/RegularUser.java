bb89883bad0bf862a2195395b8cf0849c30f2ccd
kelvindumas2 <146457744+kelvindumas2@users.noreply.github.com>
2023-12-19 20:16:27 +0000

    Update RegularUser.java
    updating class with some Exceptions

--- src/taxcalculatorapp/RegularUser.java
+++ src/taxcalculatorapp/RegularUser.java
@@ -6,52 +6,70 @@
 
     /**
      *
-     * @author thiagogoncos
+     * @author kelvindumas
      */
-    public class RegularUser {
-       
-    private String jobRole;
-    private List<TaxCalculation> taxCalculations = new ArrayList<>();
+    class RegularUser extends User {
+        private String jobRole;
+        private List<TaxCalculation> taxCalculations = new ArrayList<>();
 
-    public RegularUser(String username, String password, String name, String surname, String jobRole) {
-        super(username, password, name, surname);
-        this.jobRole = jobRole;
-    }
+        public RegularUser(String username, String password, String name, String surname, String jobRole) {
+            super(username, password, name, surname, UserType.REGULAR_USER);
+            this.jobRole = jobRole;
+        }
 
-    public String getJobRole() {
-        return jobRole;
-    }
+        public String getJobRole() {
+            return jobRole;
+        }
 
-    public void setJobRole(String jobRole) {
-        this.jobRole = jobRole;
-    }
+        public void setJobRole(String jobRole) {
+            this.jobRole = jobRole;
+        }
 
-    @Override
-    public void modifyProfile(String name, String surname) {
-        super.modifyProfile(name, surname);
-    }
+        @Override
+        public void modifyProfile(String name, String surname) {
+            super.setName(name);
+            super.setSurname(surname);
+            System.out.println("Regular user profile modified: " + this);
+        }
 
-    public void calculateAndSaveTaxes() {
+        public void calculateAndSaveTaxes() {
         Scanner scanner = new Scanner(System.in);
         TaxCalculator taxCalculator = new TaxCalculatorImpl();
 
-        System.out.print("Enter gross income: ");
-        double grossIncome = scanner.nextDouble();
+        try {
+            // Obter renda bruta
+            System.out.print("Enter gross income: ");
+            double grossIncome = scanner.nextDouble();
 
-        System.out.print("Enter tax credits: ");
-        double taxCredits = scanner.nextDouble();
+            // Obter créditos fiscais
+            System.out.print("Enter tax credits: ");
+            double taxCredits = scanner.nextDouble();
 
-        double incomeTax = taxCalculator.calculateIncomeTax(grossIncome, taxCredits);
-        double usc = taxCalculator.calculateUSC(grossIncome);
-        double prsi = taxCalculator.calculatePRSI(grossIncome);
+            // Verificar se as taxas são números
+            if (!isValidNumber(grossIncome) || !isValidNumber(taxCredits)) {
+                System.out.println("Invalid input. Please enter valid numbers.");
+                return;
+            }
 
-        System.out.println("Income Tax (PAYE): " + incomeTax);
-        System.out.println("USC: " + usc);
-        System.out.println("PRSI: " + prsi);
+            double incomeTax = taxCalculator.calculateIncomeTax(grossIncome, taxCredits);
+            double usc = taxCalculator.calculateUSC(grossIncome);
+            double prsi = taxCalculator.calculatePRSI(grossIncome);
 
-        saveTaxCalculations(grossIncome, taxCredits, incomeTax, usc, prsi);
+            System.out.println("Income Tax (PAYE): " + incomeTax);
+            System.out.println("USC: " + usc);
+            System.out.println("PRSI: " + prsi);
 
-        System.out.println("Tax calculations saved successfully!");
+            saveTaxCalculations(grossIncome, taxCredits, incomeTax, usc, prsi);
+
+            System.out.println("Tax calculations saved successfully!");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid numbers.");
+            scanner.next();
+        }
+    }
+
+    private static boolean isValidNumber(double number) {
+        return !Double.isNaN(number) && !Double.isInfinite(number);
     }
 
     private void saveTaxCalculations(double grossIncome, double taxCredits, double incomeTax, double usc, double prsi) {
@@ -59,12 +77,11 @@
         Database.saveTaxCalculations(this, taxCalculation);
     }
 
-    public List<TaxCalculation> getTaxCalculations() {
-        return taxCalculations;
-    }
+        public List<TaxCalculation> getTaxCalculations() {
+            return taxCalculations;
+        }
 
-    public void saveTaxCalculation(TaxCalculation taxCalculation) {
-        taxCalculations.add(taxCalculation);
-    }
-}
+        public void saveTaxCalculation(TaxCalculation taxCalculation) {
+            taxCalculations.add(taxCalculation);
+        }
     }
