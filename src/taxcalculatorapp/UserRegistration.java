07ba0496885ff754f7d812925924568a1bed270e
Thiago2022161 <146199077+Thiago2022161@users.noreply.github.com>
2023-12-19 17:35:17 +0000

    Update UserRegistration.java
    Updating some code to UserRegistration

--- src/taxcalculatorapp/UserRegistration.java
+++ src/taxcalculatorapp/UserRegistration.java
@@ -8,6 +8,35 @@ package taxcalculatorapp;
  *
  * @author thiagogoncos
  */
-public class UserRegistration {
-    
+class UserRegistration {
+    public static RegularUser registerUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter new username: ");
+        String username = scanner.next();
+
+        if (Database.getUser(username) != null) {
+            System.out.println("Username already exists. Please choose a different username.");
+            return null;
+        }
+
+        System.out.print("Enter new password: ");
+        String password = scanner.next();
+
+        System.out.print("Enter your name: ");
+        String name = scanner.next();
+
+        System.out.print("Enter your surname: ");
+        String surname = scanner.next();
+
+        System.out.print("Enter job role: ");
+        String jobRole = scanner.next();
+
+        RegularUser newUser = new RegularUser(username, password, name, surname, jobRole);
+
+        Database.storeUser(newUser);
+
+        System.out.println("User registration successful. Welcome, " + newUser.getUsername() + "!");
+        return newUser;
+    }
 }
