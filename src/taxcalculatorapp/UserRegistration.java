6a6528ecaf83d01ede1b1074adaf3f1383832e74
kelvindumas2 <146457744+kelvindumas2@users.noreply.github.com>
2023-12-19 20:19:45 +0000

    Update UserRegistration.java
    updating some Exceptions to the class UserRegistration

--- src/taxcalculatorapp/UserRegistration.java
+++ src/taxcalculatorapp/UserRegistration.java
@@ -6,29 +6,58 @@ package taxcalculatorapp;
 
 /**
  *
- * @author thiagogoncos
+ * @author kelvindumas
  */
 class UserRegistration {
     public static RegularUser registerUser() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter new username: ");
-        String username = scanner.next();
+        // Obter nome de usuário
+        String username;
+        do {
+            System.out.print("Enter new username (at least 8 characters): ");
+            username = scanner.next();
 
-        if (Database.getUser(username) != null) {
-            System.out.println("Username already exists. Please choose a different username.");
-            return null;
-        }
+            if (username.length() < 8) {
+                System.out.println("Username must have at least 8 characters. Please try again.");
+            }
+        } while (username.length() < 8 || Database.getUser(username) != null);
 
-        System.out.print("Enter new password: ");
-        String password = scanner.next();
+        // Obter senha
+        String password;
+        do {
+            System.out.print("Enter new password (at least 6 letters and 2 numbers): ");
+            password = scanner.next();
 
-        System.out.print("Enter your name: ");
-        String name = scanner.next();
+            // Verificar complexidade da senha
+            if (!isValidPassword(password)) {
+                System.out.println("Password must have at least 6 letters and 2 numbers. Please try again.");
+            }
+        } while (!isValidPassword(password));
 
-        System.out.print("Enter your surname: ");
-        String surname = scanner.next();
+        // Obter nome
+        String name;
+        do {
+            System.out.print("Enter your name (only letters): ");
+            name = scanner.next();
 
+            if (!isValidName(name)) {
+                System.out.println("Name must contain only letters. Please try again.");
+            }
+        } while (!isValidName(name));
+
+        // Obter sobrenome
+        String surname;
+        do {
+            System.out.print("Enter your surname (only letters): ");
+            surname = scanner.next();
+
+            if (!isValidName(surname)) {
+                System.out.println("Surname must contain only letters. Please try again.");
+            }
+        } while (!isValidName(surname));
+
+        // Obter job role
         System.out.print("Enter job role: ");
         String jobRole = scanner.next();
 
@@ -38,5 +67,25 @@ class UserRegistration {
 
         System.out.println("User registration successful. Welcome, " + newUser.getUsername() + "!");
         return newUser;
+    }
+
+    // Métodos auxiliares para validar senha e nome
+    private static boolean isValidPassword(String password) {
+        // Verifica se a senha tem pelo menos 6 letras e 2 números
+        int letterCount = 0;
+        int digitCount = 0;
+        for (char ch : password.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                letterCount++;
+            } else if (Character.isDigit(ch)) {
+                digitCount++;
+            }
+        }
+        return letterCount >= 6 && digitCount >= 2;
+    }
+
+    private static boolean isValidName(String name) {
+        // Verifica se o nome contém apenas letras
+        return name.matches("^[a-zA-Z]+$");
     }
 }
