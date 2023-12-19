bce36a003f478a5a60ca8558b7fe939a2a558a64
kelvindumas2 <146457744+kelvindumas2@users.noreply.github.com>
2023-12-19 17:24:02 +0000

    Update TaxCalculatorImpl.java
    adding values of taxes to calculate

--- src/taxcalculatorapp/TaxCalculatorImpl.java
+++ src/taxcalculatorapp/TaxCalculatorImpl.java
@@ -6,8 +6,33 @@ package taxcalculatorapp;
 
 /**
  *
- * @author thiagogoncos
+ * @author kelvindumas
  */
-public class TaxCalculatorImpl {
-    
+class TaxCalculatorImpl implements TaxCalculator {
+    @Override
+    public double calculateIncomeTax(double grossIncome, double taxCredits) {
+        double taxableIncome = grossIncome - taxCredits;
+        double incomeTaxRate = 0.20;
+        return taxableIncome * incomeTaxRate;
+    }
+
+    @Override
+    public double calculateUSC(double grossIncome) {
+        double uscRate = 0.05;
+        return grossIncome * uscRate;
+    }
+
+    @Override
+    public double calculatePRSI(double grossIncome) {
+        double prsiRate = 0.03;
+        return grossIncome * prsiRate;
+    }
+
+    @Override
+    public double calculateTotalTax(double grossIncome, double taxCredits) {
+        double incomeTax = calculateIncomeTax(grossIncome, taxCredits);
+        double usc = calculateUSC(grossIncome);
+        double prsi = calculatePRSI(grossIncome);
+        return incomeTax + usc + prsi;
+    }
 }
