07c67bd45821248cf590207f50aa6c8b739b7488
kelvindumas2 <146457744+kelvindumas2@users.noreply.github.com>
2023-12-19 20:34:30 +0000

    Update Admin.java
    updating admin

--- src/taxcalculatorapp/Admin.java
+++ src/taxcalculatorapp/Admin.java
@@ -9,28 +9,58 @@ package taxcalculatorapp;
  * @author kelvidumas
  */
 class Admin extends User {
-    private String adminTitle;
+	private String adminTitle;
 
-    public Admin(String username, String password, String name, String surname) {
-        super(username, password, name, surname, UserType.ADMIN);
-    }
+	public Admin(String username, String password, String name, String surname) {
+    	super(username, password, name, surname, UserType.ADMIN);
+	}
 
-    public String getAdminTitle() {
-        return adminTitle;
-    }
+	public String getAdminTitle() {
+    	return adminTitle;
+	}
 
-    public void setAdminTitle(String adminTitle) {
-        this.adminTitle = adminTitle;
-    }
+	public void setAdminTitle(String adminTitle) {
+    	this.adminTitle = adminTitle;
+	}
 
-    @Override
-    public void modifyProfile(String name, String surname) {
-        super.setName(name);
-        super.setSurname(surname);
-        System.out.println("Admin profile modified: " + this);
-    }
+	@Override
+	public void modifyProfile(String name, String surname) {
+    	super.setName(name);
+    	super.setSurname(surname);
+    	System.out.println("Admin profile modified: " + this);
+	}
 
-    public void grantAdminPrivileges(RegularUser user) {
-        System.out.println("Admin privileges granted to user: " + user.getUsername());
-    }
+	public void grantAdminPrivileges(RegularUser user) {
+    	System.out.println("Admin privileges granted to user: " + user.getUsername());
+	}
+
+   private static void reviewOperations(Admin admin) {
+	Scanner scanner = new Scanner(System.in);
+
+	for (User user : Database.getAllUsers()) {
+    	if (user instanceof RegularUser) {
+        	RegularUser regularUser = (RegularUser) user;
+        	System.out.println("Tax calculations for user " + regularUser.getUsername() + ":");
+
+        	System.out.print("Enter gross income for user " + regularUser.getUsername() + ": ");
+        	double grossIncome = scanner.nextDouble();
+        	System.out.print("Enter tax credits for user " + regularUser.getUsername() + ": ");
+        	double taxCredits = scanner.nextDouble();
+
+        	regularUser.calculateAndSaveTaxes(grossIncome, taxCredits);
+
+        	for (TaxCalculation taxCalculation : regularUser.getTaxCalculations()) {
+            	System.out.println(taxCalculationToString(taxCalculation));
+        	}
+    	}
+	}
+}
+
+	private static String taxCalculationToString(TaxCalculation taxCalculation) {
+    	return "Gross Income: " + taxCalculation.getGrossIncome() +
+           	", Tax Credits: " + taxCalculation.getTaxCredits() +
+           	", Income Tax: " + taxCalculation.getIncomeTax() +
+           	", USC: " + taxCalculation.getUsc() +
+           	", PRSI: " + taxCalculation.getPrsi();
+	}
 }
